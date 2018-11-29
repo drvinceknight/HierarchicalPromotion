3c5c742ddbb6b743434bd7466ad2ed54ce87b942
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2018-11-29 13:46:00 +0000

    write initial software framework (#7)
    * write initial software framework
    
    * Add documentation.
    
    Also fix environment.yml call to pip.
    
    Closes #3

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+# Read in the version number
+exec(open('src/hierarchy/version.py', 'r').read())
+
+setup(
+    name="hierarchy",
+    version=__version__,
+    # install_requires=requirements,
+    author="Vince Knight and Nikoleta E. Glynatsi",
+    author_email=("knightva@cardiff.ac.uk"),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="",
+    license="The MIT License (MIT)",
+)
