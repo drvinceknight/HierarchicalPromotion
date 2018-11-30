6717f8e6946f291e5ea4de8a04ee00cd8475df71
Vince Knight <vincent.knight@gmail.com>
2018-11-30 14:43:09 +0000

    Update state space and add enumeration.
    I have corrected the state space definition, I have also added a
    function that carries out the calculatons and included tests (which
    essentially test the mathematics).

--- src/hierarchy/states/__init__.py
+++ src/hierarchy/states/__init__.py
@@ -1,1 +1,1 @@
-from .states import get_level_states, get_states
+from .states import get_level_states, get_states, enumerate_states
--- tests/test_states.py
+++ tests/test_states.py
@@ -22,3 +22,16 @@ def test_get_level_states():
     states = list(states_generators)
     assert states == [(0, 3), (1, 2), (2, 1), (3, 0),
                       (0, 2), (1, 1), (2, 0)]
+
+def test_enumerate_states():
+    capacities = [2, 1]
+    states = list(hrcy.states.get_states(capacities))
+    assert len(states) == hrcy.states.enumerate_states(capacities)
+
+    capacities = [4, 3, 1]
+    states = list(hrcy.states.get_states(capacities))
+    assert len(states) == hrcy.states.enumerate_states(capacities)
+
+    capacities = [5, 4, 2, 1]
+    states = list(hrcy.states.get_states(capacities))
+    assert len(states) == hrcy.states.enumerate_states(capacities)
