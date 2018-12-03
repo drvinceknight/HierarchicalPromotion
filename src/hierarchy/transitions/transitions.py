936fd535457091da4fa2e67469d1f8f126fcb3fb
Vince Knight <vincent.knight@gmail.com>
2018-12-03 14:27:43 +0000

    Add code to generate potential states.

--- src/hierarchy/transitions/transitions.py
+++ src/hierarchy/transitions/transitions.py
@@ -29,7 +29,7 @@ def get_rate(
 
     if len(delta_indices) == 1:
         i, j = delta_indices[0]
-        if (delta[i, j] == -1 
+        if (delta[i, j] == -1
             and np.array_equal(np.sum(state_in, axis=1), capacities)):
             return mu[i][j]
 
@@ -39,9 +39,9 @@ def get_rate(
 
     if len(delta_indices) == 2:
         i, j = delta_indices[1]
-        if (i > 0 
-            and sum(state_in[i]) < capacities[i] 
-            and delta[i, j] == 1 
+        if (i > 0
+            and sum(state_in[i]) < capacities[i]
+            and delta[i, j] == 1
             and delta[i - 1, j] == -1):
             return r * state_in[i, j] + state_in[i, (j + 1) % 2]
 
@@ -69,3 +69,42 @@ def get_transition_matrix(capacities, r, lmbda, mu, digits=7):
     for i in range(size):
         matrix[i, i] = - sum(matrix[i])
     return np.round(matrix, digits)
+
+def get_potential_states(state_in, capacities):
+    """
+    Return tuple of all potential next states for a given `state_in`
+    """
+    state_in = np.array(state_in)
+    number_of_levels = len(capacities)
+    states = []
+    if is_full(state_in=state_in, capacities=capacities):
+        for level in range(number_of_levels - 1):  # No retirement at top level
+            for column in (0, 1):
+                if state_in[level, column] > 0:
+                    adjustment = np.zeros((number_of_levels, 2))
+                    adjustment[level, column] = -1
+                    states.append(state_in + adjustment)
+        return states
+
+    for level in find_free_levels(state_in=state_in, capacities=capacities):
+        for column in (0, 1):
+            adjustment = np.zeros((number_of_levels, 2))
+            adjustment[level, column] = 1
+            states.append(state_in + adjustment)
+    return states
+
+
+def is_full(state_in, capacities):
+    """
+    A boolean to check if a given state is full.
+    """
+    return np.array_equal(np.sum(state_in, axis=1), capacities)
+
+
+def find_free_levels(state_in, capacities):
+    """
+    A generator of the levels that have capacity
+    """
+    for level, (row, capacity) in enumerate(zip(state_in, capacities)):
+        if np.sum(row) < capacity:
+            yield level
