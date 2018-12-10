9dd771f269258641ba8ac9681136fe10d0034e95
Vince Knight <vincent.knight@gmail.com>
2018-12-10 12:13:53 +0000

    Add code to generate potential states. (#11)

--- src/hierarchy/transitions/__init__.py
+++ src/hierarchy/transitions/__init__.py
@@ -1,1 +1,1 @@
-from .transitions import get_rate, get_transition_matrix
+from .transitions import get_rate, get_transition_matrix, get_potential_states, is_full, find_free_levels
--- tests/test_transitions.py
+++ tests/test_transitions.py
@@ -92,3 +92,47 @@ def test_get_transition_matrix_example_two():
     matrix = hrcy.transitions.get_transition_matrix(capacities=capacities, r=r, lmbda=lmbda, mu=mu)
     assert np.array_equal(matrix.shape, np.array([90, 90]))
     assert np.allclose(np.sum(matrix, axis=1), 0)
+
+
+def test_get_potential_states_hire():
+    capacities = [4, 2, 1]
+    state_in = [[2, 1], [2, 0], [1, 0]]
+    potential_states = hrcy.transitions.get_potential_states(state_in=state_in, capacities=capacities)
+    expected_states = ([[3, 1], [2, 0], [1, 0]], [[2, 2], [2, 0], [1, 0]])
+    assert all(np.array_equal(potential, expected) for potential, expected in zip(potential_states, expected_states))
+
+def test_get_potential_states_promotion():
+    capacities = [4, 2, 1]
+    state_in = [[2, 2], [1, 0], [1, 0]]
+    potential_states = hrcy.transitions.get_potential_states(state_in=state_in, capacities=capacities)
+    expected_states = ([[2, 2], [2, 0], [1, 0]], [[2, 2], [1, 1], [1, 0]])
+    assert all(np.array_equal(potential, expected) for potential, expected in zip(potential_states, expected_states))
+
+def test_get_potential_states_retirement():
+    capacities = [4, 2, 1]
+    state_in = [[2, 2], [1, 1], [1, 0]]
+    potential_states = hrcy.transitions.get_potential_states(state_in=state_in, capacities=capacities)
+    expected_states = ([[1, 2], [1, 1], [1, 0]], 
+                       [[2, 1], [1, 1], [1, 0]], 
+                       [[2, 2], [0, 1], [1, 0]], 
+                       [[2, 2], [1, 0], [1, 0]], 
+                       )
+    assert len(expected_states) == len(potential_states)
+    assert all(np.array_equal(potential, expected) for potential, expected in
+        zip(potential_states, expected_states))
+
+def test_is_full():
+    capacities = [4, 2, 1]
+    state_in = [[2, 2], [2, 0], [1, 0]]
+    assert hrcy.transitions.is_full(state_in=state_in, capacities=capacities)
+
+    state_in = [[2, 2], [1, 0], [1, 0]]
+    assert not hrcy.transitions.is_full(state_in=state_in, capacities=capacities)
+
+def test_find_free_levels():
+    capacities = [4, 2, 1]
+    state_in = [[2, 2], [2, 0], [1, 0]]
+    assert list(hrcy.transitions.find_free_levels(state_in=state_in, capacities=capacities)) == []
+
+    state_in = [[2, 2], [1, 0], [1, 0]]
+    assert list(hrcy.transitions.find_free_levels(state_in=state_in, capacities=capacities)) == [1]
