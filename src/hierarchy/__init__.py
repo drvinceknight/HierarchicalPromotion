2f62a2eb21f963af8b77903fcb10433f24b7bcf6
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-12-10 21:25:54 +0000

    add test file for draw

--- src/hierarchy/__init__.py
+++ src/hierarchy/__init__.py
@@ -6,4 +6,4 @@ from .version import __version__
 from .stationary import get_stationary_distribution
 from .simulation import get_simulated_history
 
-from .draw import states_to_tikz
+from .draw import state_to_tikz
