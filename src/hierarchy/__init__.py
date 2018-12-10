38996f8342a419f69bb0e2b8be39286f89fd5a50
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-12-10 15:57:36 +0000

    address comments on pr
    Change the name of the function which
    now takes states and returns a string.

--- src/hierarchy/__init__.py
+++ src/hierarchy/__init__.py
@@ -6,4 +6,4 @@ from .version import __version__
 from .stationary import get_stationary_distribution
 from .simulation import get_simulated_history
 
-from .represent_system import draw_system_using_tikz
+from .represent_system import states_to_tikz
--- src/hierarchy/represent_system.py
+++ src/hierarchy/represent_system.py
@@ -16,37 +16,41 @@ tail = r"""
 \end{tikzpicture}
 \end{document}"""
 
-def color_distribution(capacities, of_type_one):
-    distributions = []
-    for capacity, number_of_type_one in zip(capacities, of_type_one):
-        distribution = ['blue' for _ in range(number_of_type_one)]
-        distribution += ['red' for _ in range(capacity - number_of_type_one)]
-
-        random.shuffle(distribution)
-        distributions.append(distribution)
-    return distributions
-
-def draw_system_using_tikz(capacities, of_type_one, filename):
+def states_to_tikz(capacities, states, include_edges=False, include_boiler_plate=False):
     write_nodes = ""
     node_number = 0
     nodes_sets = []
-    colors = color_distribution(capacities=capacities, of_type_one=of_type_one)
-
-    for layer, capacity in enumerate(capacities):
+    for layer, state in enumerate(states):
+        nodes_in_layer = 0
         nodes_subset = []
-        for indv, color in zip(range(capacity), colors[layer]):
-            write_nodes += r"\node[circle, draw=black, fill=%s]  (%s) at (%s, %s) {};" 
-                                      % (color, node_number, indv, layer) + "\n"
+        for type_i in range(state[0]):
+            write_nodes += r"\node[circle, draw=black, dashed, fill=blue]  (%s) at (%s, %s) {};" % (node_number, nodes_in_layer, layer) + "\n"
             nodes_subset.append(node_number)
             node_number += 1
+            nodes_in_layer += 1
+        
+        for type_j in range(state[1]):
+            write_nodes += r"\node[circle, draw=black, dotted, fill=red]  (%s) at (%s, %s) {};" % (node_number, nodes_in_layer, layer) + "\n"
+            nodes_subset.append(node_number)
+            node_number += 1
+            nodes_in_layer += 1
+            
+        while nodes_in_layer  < capacities[layer]:
+            write_nodes += r"\node[circle, draw=black]  (%s) at (%s, %s) {};" % (node_number, nodes_in_layer, layer) + "\n"
+            nodes_subset.append(node_number)
+            node_number += 1
+            nodes_in_layer += 1
         nodes_sets.append(nodes_subset)
 
     write_edges = ""
-    for i, _ in enumerate(nodes_sets[:-1]):
-        for pair in itertools.product(nodes_sets[i], nodes_sets[i + 1]):
-            write_edges += r"\draw (%s) -- (%s);" % (pair) + "\n"
+    if include_edges:
+        for i, _ in enumerate(nodes_sets[:-1]):
+            for pair in itertools.product(nodes_sets[i], nodes_sets[i + 1]):
+                write_edges += r"\draw (%s) -- (%s);" % (pair) + "\n"
 
-    file = open(filename,"w")
-    for lines in [head, write_nodes, write_edges, tail]:
-        file.write(lines)
-    file.close()
+    tikz_code = write_nodes + write_edges
+
+    if include_boiler_plate:
+        tikz_code = head + tikz_code + tail
+
+    return tikz_code
