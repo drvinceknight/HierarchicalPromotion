22b430c922caff9fb6946fd69f6c0a436d0b35d1
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-09-23 13:42:26 +0100

    adds test for potential states

--- tests/test_competence_transitions.py
+++ tests/test_competence_transitions.py
@@ -176,15 +176,39 @@ def test_get_potential_states_retirement():
     )
 
 
-# # def test_get_potential_states_promotion_all_capacities_one():
-# #     capacities = [1, 1, 1]
-# #     state_in = [[0, 1], [0, 0], [0, 1]]
-# #     potential_states = hrcy.transitions.get_potential_states(
-# #         state_in=state_in, capacities=capacities
-# #     )
-# #     expected_states = ([[0, 0], [0, 1], [0, 1]],)
-# #     assert len(expected_states) == len(potential_states)
-# #     assert all(
-# #         np.array_equal(potential, expected)
-# #         for potential, expected in zip(potential_states, expected_states)
-# #     )
+def test_get_potential_states_promotion_all_capacities_one():
+    capacities = [1, 1, 1]
+    distribution = scipy.stats.uniform(0, 1)
+    state_in = [
+        [
+            hrcy.states.Individual(
+                individual_type=1, competence_distribution=distribution
+            )
+        ],
+        [None],
+        [
+            hrcy.states.Individual(
+                individual_type=0, competence_distribution=distribution
+            )
+        ],
+    ]
+
+    potential_state = hrcy.transitions.get_competence_potential_states(
+        state_in=state_in,
+        capacities=capacities,
+        competence_distribution=distribution,
+    )
+    states = list(potential_state)
+    potential_types_in_states = [
+        hrcy.transitions.get_types_in_state_from_competence_state(level)
+        for level in states
+    ]
+
+    expected_states = [([[0, 0], [0, 1], [1, 0]])]
+    assert len(expected_states) == len(potential_types_in_states)
+    assert all(
+        np.array_equal(potential, expected)
+        for potential, expected in zip(
+            potential_types_in_states, expected_states
+        )
+    )
