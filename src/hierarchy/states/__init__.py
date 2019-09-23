ff13f41514cdaf749387e5b58446b13f2d24cb2b
Vince Knight <vincent.knight@gmail.com>
2019-09-23 10:20:23 +0100

    Write class for individual (#29)
    * Write failing test for individual.
    
    * Write init method for Individual class.

--- src/hierarchy/states/__init__.py
+++ src/hierarchy/states/__init__.py
@@ -1,1 +1,2 @@
 from .base.states import get_level_states, get_states, enumerate_states
+from .competence import Individual
---
+++ tests/test_competence_individual.py
@@ -0,0 +1,27 @@
+import itertools
+import types
+import scipy.stats
+import numpy as np
+
+import hierarchy as hrcy
+
+
+def test_individual_init():
+    distribution = scipy.stats.uniform(0, 1)
+    np.random.seed(0)
+    individual = hrcy.states.Individual(individual_type=0,
+            competence_distribution=distribution)
+    assert individual.individual_type == 0
+    assert np.isclose(individual.competence, 0.5488135039273248)
+
+    np.random.seed(1)
+    individual = hrcy.states.Individual(individual_type=0,
+            competence_distribution=distribution)
+    assert individual.individual_type == 0
+    assert np.isclose(individual.competence,  0.417022004702574)
+
+    np.random.seed(1)
+    individual = hrcy.states.Individual(individual_type=1,
+            competence_distribution=distribution)
+    assert individual.individual_type == 1
+    assert np.isclose(individual.competence,  0.417022004702574)
