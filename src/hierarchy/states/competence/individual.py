7b589cc82e65fba4d6be76f7766a4b9d592c0b1b
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-09-23 15:55:31 +0100

    add retirement date to individual
    tests are currently failing

--- src/hierarchy/states/competence/individual.py
+++ src/hierarchy/states/competence/individual.py
@@ -1,11 +1,23 @@
+import numpy as np
+
+
 class Individual:
     """
     A class for an individual in a competence based model
     """
 
-    def __init__(self, individual_type, competence_distribution):
+    def __init__(
+        self,
+        individual_type,
+        competence_distribution,
+        retirement_rate,
+        last_retirement,
+    ):
         self.individual_type = individual_type
         self.competence = competence_distribution.rvs()
+        self.retirement_date = last_retirement + np.random.exponential(
+            retirement_rate
+        )
 
     def __str__(self):
         return f"{self.individual_type}, {self.competence}"
