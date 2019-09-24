43e646552884d94e2a124572604301ffc1c21681
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-09-24 18:29:41 +0100

    implement measures

--- src/hierarchy/__init__.py
+++ src/hierarchy/__init__.py
@@ -1,6 +1,7 @@
 import hierarchy.simulation
 import hierarchy.states
 import hierarchy.transitions
+import hierarchy.measures
 
 from .draw import state_to_tikz
 from .stationary import get_stationary_distribution
